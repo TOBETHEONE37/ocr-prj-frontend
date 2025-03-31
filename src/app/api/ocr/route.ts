955dbd9c19e2e2f8c56f62d93664d6f50b92900e
chohw37 <chohw37@tobetheone.kr>
2025-03-31 10:54:41 +0900

    modify

--- src/app/api/ocr/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(req: Request) {
-  const data = await req.formData();
-  const image = data.get('image');
-
-  return NextResponse.json({ success: true });
-}
